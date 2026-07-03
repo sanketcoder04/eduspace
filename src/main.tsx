c059f2c478102e6c717e46847091826449e295c2
Sanket Ganguly <sanketganguly53@gmail.com>
2026-07-04 00:53:27 +0530

    AntD setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider } from "antd";
+import App from "./App";
 import "./styles/globals.css";
-import App from "./App.tsx";
+import { antdTheme } from "./theme";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ConfigProvider theme={antdTheme}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
 );
